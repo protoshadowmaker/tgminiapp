cc800dacfba9d61838886f8b6fd5917fa7e0516d
protoshadowmaker <134295262+protoshadowmaker@users.noreply.github.com>
2023-09-27 17:13:16 +0200

    Add files via upload

--- page-main.js
+++ page-main.js
@@ -2,15 +2,44 @@
 
 function displayMainPage() {
     replaceTopPage("main-page", mainPage());
+    displayApps();
 }
 
 function mainPage() {
     return `
     <h2>Loading page</h2>
     <p>Wait until the data has been loaded.</p>
+    <div id="apps-container">
     ${appItemView("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/240px-Telegram_logo.svg.png", "Title", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.")}
     ${appItemView("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/240px-Telegram_logo.svg.png", "Title", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.")}
     ${appItemView("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/240px-Telegram_logo.svg.png", "Title", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.")}
     ${appItemView("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/240px-Telegram_logo.svg.png", "Title", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.")}
+    </div>
     `;
+}
+
+function displayApps() {
+    //clear items
+    let displayContent = "";
+    for (let i = 0; i < 100; i++) {
+        displayContent += `\n${appItemView("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/240px-Telegram_logo.svg.png", "Title", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.")}`
+    }
+    replaceInElement("apps-container", displayContent);
+}
+
+function loadApps(category) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
 }
